b1dc7f8cda28b7f5ca36ac803e4e61ac92cf2105
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2019-05-21 02:36:18 +0530

    Updated Packages

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
                 version: '71.0',
                 name: 'Karma With Heartbeat',
                 tunnel: true, // In case karma is running on local machine
-                tunnelName:'jasmine', // In case running multiple tunnel
+               // tunnelName:'jasmine', // In case running multiple tunnel
                 video: true, // capture video for your test
                 visual: true, // capture screenshots on each step
                 network: true, // capture network logs for your test
